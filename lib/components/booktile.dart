7ca57301f99e89014bba449d8997531f7f72aa51
belfarz <115107231+belfarz@users.noreply.github.com>
2024-03-20 00:10:39 +0100

    many functionalies later

--- lib/components/booktile.dart
+++ lib/components/booktile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BookTile extends StatelessWidget {
   final void Function()? onTap;
@@ -14,68 +15,47 @@ class BookTile extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        margin:const EdgeInsets.only(left: 25),
-        padding: EdgeInsets.all(25),
-        
-        decoration: BoxDecoration(
-            color: Colors.grey[100], borderRadius: BorderRadius.circular(20)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-      
-            // SizedBox(height: 100,),
-            //image
-            Image.network(
-              book["Image"],
-              height: 140,
-            ),
-            const SizedBox(height: 20,),
-            //text
-            Text(
-              book["Name"],
-              style:const TextStyle(
-                fontSize: 20
-              ),
-            ),
-      
-            //price + rating
-            SizedBox(
-              width: 160,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  //price
-                  Text(
-                    "\$"+book["Price"].toString(),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[700],
+              padding: const EdgeInsets.all(10.0),
+              child: Material(
+                elevation: 3,
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(5),
+                          child: Image.network(book["Image"], height: 170, width: 150, fit: BoxFit.cover,),
+                        ),
+                        Text(book["Name"], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                        SizedBox(height: 5,),
+                        Text(book["Author"], 
+                          style: TextStyle(
+                            color: Colors.black38, 
+                            fontSize: 15, 
+                            fontWeight: FontWeight.w500),),
+                        SizedBox(height: 5,),
+                        Row(
+                          children: [
+                            Text("\$" + book["Price"], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                            SizedBox(
+                              width: 40,
+                            ),
+                            Text("${book["Rating"] ?? "Not rated"}", style: TextStyle(fontSize: 18),), // Display rating or "Not rated" if rating is not available
+                        Icon(Icons.star_rounded, color: Colors.amber, size: 25,),
+                          ],
+                        ),
+                        
+                      ],
                     ),
                   ),
-      
-                  //icon
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star,
-                        color: Colors.yellow[800],
-                      ),
-                      //rating
-                      Text(
-                        book["Rating"].toString(),
-                        style: TextStyle(color: Colors.grey),
-                        ),
-                    ],
-                  ),
-      
-                ],
+                ),
               ),
-            )
-      
-          ],
-        ),
-      ),
+            ),
     );
   }
 }
+
+
